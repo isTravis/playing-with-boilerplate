b58d876214cd686cdad278ec97d7b846e6428175
Travis Rich <travis.s.rich@gmail.com>
2017-05-17 11:10:08 -0400

    Add nav bar

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -12,8 +12,29 @@ class NavBar extends Component {
 	}
 
 	render() {
+		const toggleSlide = function() {
+			const elem = document.getElementsByClassName('nav-menu')[0];
+			if (elem.className === 'nav-menu') {
+				elem.className = 'nav-menu open';
+			} else {
+				elem.className = 'nav-menu';
+			}
+		};
+
 		return (
-			<div id="navbar-page">Nav Bar</div>
+			<div id="navbar-page">
+				<div onClick={toggleSlide}>Nav Bar</div>
+				<div className={'nav-menu'} onClick={toggleSlide}>
+					<div className={'content'}>
+						<ul>
+							<li>Item 1</li>
+							<li>Item 2</li>
+							<li>Item 3</li>
+							<li>Item 4</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
